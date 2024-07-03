737da4407c3669f3718fda73295d233f4660e7da
Jio7g <cramosg8@miumg.edu.gt>
2024-07-03 11:45:57 -0600

    011: mejoras: Mejoras generales en los estilos de la página principal

--- js/index-app.js
+++ js/index-app.js
@@ -33,58 +33,54 @@ new Vue({
     // Array de objetos con información de los servicios
     servicios: [
       {
-        titulo: "Servicio de Fletes",
-        icono: "fas fa-truck",
-        descripcion:
-          "Ofrecemos un servicio de fletes para entregarte los materiales directamente en la obra.",
-        detalle:
-          "Contamos con vehículos preparados para transportar todo tipo de materiales de construcción de manera segura y eficiente.",
+          titulo: "Servicio de Fletes",
+          icono: "fas fa-truck",
+          descripcion: "Ofrecemos un servicio de fletes para entregarte los materiales directamente en la obra.",
+          detalle: "Contamos con vehículos preparados para transportar todo tipo de materiales de construcción de manera segura y eficiente.",
+          expandido: false
       },
       {
-        titulo: "Herramientas Profesionales",
-        icono: "fas fa-tools",
-        descripcion:
-          "Contamos con una amplia selección de herramientas profesionales de las mejores marcas.",
-        detalle:
-          "Nuestro equipo te ayudará a elegir las herramientas adecuadas para tus proyectos y necesidades específicas.",
+          titulo: "Herramientas Profesionales",
+          icono: "fas fa-tools",
+          descripcion: "Contamos con una amplia selección de herramientas profesionales de las mejores marcas.",
+          detalle: "Nuestro equipo te ayudará a elegir las herramientas adecuadas para tus proyectos y necesidades específicas.",
+          expandido: false
       },
       {
-        titulo: "Artículos para el Hogar",
-        icono: "fas fa-home",
-        descripcion:
-          "Encuentra una gran variedad de artículos para el hogar y proyectos de mejora.",
-        detalle:
-          "Desde accesorios de baño hasta decoración y muebles, tenemos todo lo que necesitas para hacer de tu hogar un lugar acogedor.",
+          titulo: "Artículos para el Hogar",
+          icono: "fas fa-home",
+          descripcion: "Encuentra una gran variedad de artículos para el hogar y proyectos de mejora.",
+          detalle: "Desde accesorios de baño hasta decoración y muebles, tenemos todo lo que necesitas para hacer de tu hogar un lugar acogedor.",
+          expandido: false
       },
       {
-        titulo: "Materiales de Construcción",
-        icono: "fa-solid fa-person-digging",
-        descripcion:
-          "Ofrecemos una amplia gama de materiales de construcción de alta calidad.",
-        detalle:
-          "Desde cemento y block hasta las mejores herramientas de construcción, tenemos todo lo que necesitas para tus proyectos de construcción y remodelación. Garantizamos productos duraderos y a precios accesibles.",
-      },
+          titulo: "Materiales de Construcción",
+          icono: "fa-solid fa-person-digging",
+          descripcion: "Ofrecemos una amplia gama de materiales de construcción de alta calidad.",
+          detalle: "Desde cemento y block hasta las mejores herramientas de construcción, tenemos todo lo que necesitas para tus proyectos de construcción y remodelación.",
+          expandido: false
+      }
     ],
     // Array de objetos con los valores de la empresa
     companyValues: [
       {
-        title: "Calidad",
-        icon: "fas fa-award",
-        description:
-          "Ofrecemos productos de la más alta calidad para nuestros clientes.",
+          title: "Calidad",
+          icon: "fas fa-award",
+          description: "Ofrecemos productos de la más alta calidad para nuestros clientes.",
+          additionalInfo: "Trabajamos con las mejores marcas y materiales."
       },
       {
-        title: "Servicio",
-        icon: "fas fa-handshake",
-        description:
-          "Nos comprometemos a brindar un servicio excepcional en cada interacción con nuestros clientes.",
+          title: "Servicio",
+          icon: "fas fa-handshake",
+          description: "Nos comprometemos a brindar un servicio excepcional en cada interacción con nuestros clientes.",
+          additionalInfo: "Nuestro equipo estrá a su diposición para ofrecerle asesoramiento."
       },
       {
-        title: "Integridad",
-        icon: "fas fa-shield-alt",
-        description:
-          "Actuamos con honestidad y transparencia en todo lo que hacemos.",
-      },
+          title: "Integridad",
+          icon: "fas fa-shield-alt",
+          description: "Actuamos con honestidad y transparencia en todo lo que hacemos.",
+          additionalInfo: "La confianza de nuestros clientes es el fundamento de nuestro éxito."
+      }
     ],
     // Array de objetos con información de las marcas
     marcas: [
@@ -149,7 +145,15 @@ new Vue({
         }
       }, 100);
     },
-  },// Ciclo de vida: cuando el componente se monta en el DOM
+    // Método para expandir y contraer el detalle de un servicio
+    expandServicio(index) {
+      this.servicios[index].expandido = true;
+    },
+    contraerServicio(index) {
+        this.servicios[index].expandido = false;
+    },
+  },
+  // Ciclo de vida: cuando el componente se monta en el DOM
   mounted() {
     // Agregar evento de scroll
     window.addEventListener("scroll", this.handleScroll);
